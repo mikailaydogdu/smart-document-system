8b0acfc19cbeb8f5053d329f0291a83fbe0b1199
engin <erginas@gmail.com>
2019-07-23 11:43:59 +0300

    users add

--- src/documents/models.py
+++ src/documents/models.py
@@ -1,30 +1,38 @@
 from django.db import models
-from src.users.models import UserTable
+from users.models import UserTable
+
 
 # Create your models here.
 class my_codes(models.Model):
-    pass
+    """
+    Kodların tutulduğu tablom burada
+    """
+    type    = models.CharField(max_length=32, verbose_name='Dokuman Turu')
+    code    = models.CharField(max_length=16, verbose_name='Kısa Kodu')
+    title   = models.CharField(max_length=32, verbose_name='Kod Açıklaması')
 
 
 class Documents(models.Model):
     """
-    Document Master table
+    Document Master table dökümana ait genel bilgilerin bulunduğu table
     """
-    doc_name    = models.CharField(max_length=36, verbose_name='Documnet Name')
-    doc         = models.FileField()
-    doc_type    = models.CharField(max_length=36)
-    doc_date    = models.DateTimeField(auto_now_add=True)
-
-
-    pass
-
-# class Document_Type(models.Model):
-#     pass
+    title       = models.CharField(max_length=36, verbose_name='Döküman Başlık')
+    doc         = models.FileField(verbose_name='Dosya')
+    type        = models.CharField(max_length=36, verbose_name='Dosta Türü')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Dokuman Ekleme Tarihi')
+    active      = models.BooleanField(default=True)
 
 class Documents_Hash(models.Model):
-    doc_id      = models.ForeignKey('Documents',on_delete=models.CASCADE, verbose_name='document_id')
+    """
+    Document hash table yeni bir döküman ekleme sırasın eklenen dökümanla var olan döküman ın
+    hash bilgisine bakmak için gerekli gördüğüm table
+    """
+    doc_id      = models.ForeignKey(Documents,on_delete=models.CASCADE, verbose_name='document_id')
     doc_hash    = models.CharField(max_length=64, verbose_name='document_hash')
 
 class Documents_user(models.Model):
-    user_id = models.ForeignKey('UserTable', on_delete=models.CASCADE)
-    doc_id = models.ForeignKey('Documents', on_delete=models.CASCADE)
+    """
+    Kullanıcıların eklemiş oldğu ve kullanıcılara göre dökümanlara ulaşmak için kullanıdığım table
+    """
+    user_id     = models.ForeignKey(UserTable, on_delete=models.CASCADE)
+    doc_id      = models.ForeignKey(Documents, on_delete=models.CASCADE)
--- src/users/models.py
+++ src/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class UserTable(AbstractBaseUser):
-    birth_date  = models.DateField()
-    telephone   = models.CharField(max_length=11)
-    lldatetime    = models.DateTimeField(null=True)
-    image       = models.ImageField()
-    location    = models.CharField(max_length=32,null=True)
-    biographi   = models.TextField(max_length=280, null=True)
+class UserTable(AbstractUser):
+    birth_date  = models.DateField(blank=True, verbose_name='Doğum Tarihi')
+    telephone   = models.CharField(max_length=11, blank=True, verbose_name='Telefon No')
+    image       = models.ImageField(blank=True, verbose_name='Resim')
+
+    # def generate_password(self):
+    #     self.password
